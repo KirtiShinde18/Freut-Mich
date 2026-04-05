d31b4fe94723905eb854da8d6efd096bb170b8a8
kirti <kirtishinde3520@gmail.com>
2026-04-05 21:21:31 +0530

    text changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Footer = () => {
-  return <div className='text-center p-5 bg-[#fef6ef]'>
+  return <div className='text-center p-5 bg-[#fef6ef] text-black'>
    © 2026 copyright .  All rights Reserved. Design by <p className="bg-gradient-to-r from-[#713827] via-[#bc7d49] to-[#d3aa82] bg-clip-text text-transparent"><b>Kuldeep Websoft Pvt.Ltd.</b></p>
   </div>
 }
